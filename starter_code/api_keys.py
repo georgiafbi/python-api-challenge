d099098c7ae6ac5e44f30b15e4a891dc4b1ca6db
Destany Brown <Georgiafbi@gmail.com>
2021-02-02 06:03:39 -0500

    waiting on api key to go live

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "f3685959723c0da772b7425c49a6e453"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyA446whaNBzu7aEOH_SFoLpo73T0Nr_Nn8"
